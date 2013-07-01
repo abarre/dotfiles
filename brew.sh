99ef98a626d7dbcf5dd9ec3e9d49a450c0d3ad5b
abarre <anthony.barre87@gmail.com>
2013-07-01 18:35:25 +0200

    add brew installation.

---
+++ brew.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# Install wget with IRI support
+brew install wget --enable-iri
+
+# Install other useful binaries
+brew install ack
+brew install git
+brew install pigz
+brew install tree
+brew install webkit2png
+
+# Install native apps
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+function installcask() {
+	if brew cask info "${@}" | grep "Not installed" > /dev/null; then
+		brew cask install "${@}"
+	else
+		echo "${@} is already installed."
+	fi
+}
+
+installcask diffmerge
+installcask evernote
+installcask dropbox
+installcask google-chrome
+installcask iterm2
+installcask sublime-text
+installcask the-unarchiver
+installcask virtualbox
+installcask vlc
+installcask adium
+installcask gitx
+
+# Remove outdated versions from the cellar
+brew cleanup
